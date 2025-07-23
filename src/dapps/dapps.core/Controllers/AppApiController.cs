8056fbdec0de79323ac484ed177d0cf668d0eb79
Tom Fanning <tom@m0lte.uk>
2025-07-23 23:30:46 +0100

    Create AppApiController.cs

---
+++ src/dapps/dapps.core/Controllers/AppApiController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dapps.core.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AppApiController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("App API is running");
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] Message value)
+    {
+        // Here you can handle the posted value
+        return Ok($"Received value: {value}");
+    }
+}
+
+public readonly record struct Message
+{
+    public string FromCall { get; init; }
+    public string ToNode { get; init; }
+    public string ToApp { get; init; }
+    public DateTime Ttl { get; init; }
+    public byte[] Payload { get; init; }
+}
